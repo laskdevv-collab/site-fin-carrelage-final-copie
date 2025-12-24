e669ef5ce29b645f770052f0259b0baa94fa69b5
halilichimaru <halilichimaru@gmail.com>
2025-12-24 23:48:44 +0000

    zzzzzzzzzzzzz

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,36 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/images/icon.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate'
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache'
+          },
+          {
+            key: 'Expires',
+            value: '0'
+          }
+        ]
+      }
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/favicon.ico',
+        destination: '/images/icon.ico',
+        permanent: false
+      }
+    ];
+  }
 };
 
 export default nextConfig;
